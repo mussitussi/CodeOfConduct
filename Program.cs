0074d82cb3cd9176996568b2a5759acc3403eaa4
Mikkel Strange <mikkel.strange@gmail.com>
2021-03-04 08:21:55 +0100

    program main and bond

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ImmutableDemo
+{
+    public class Bond
+    {
+        public string Isin { get; }
+        public double Price { get; }
+        public Bond(string isin, double price) => (Isin, Price) = (isin, price);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new Bond("DK123", 100.5);
+        }
+    }
+}
